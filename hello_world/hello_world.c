4ef16d0bb5942db31d97be4a02c06dc4b0fadfc2
matteotesori <matteotesori@gmail.com>
2018-03-11 23:08:35 +0100

    First commit by Matteo Tesori

---
+++ hello_world/hello_world.c
@@ -0,0 +1,12 @@
+/*
+ * hello_world.c
+ *
+ *  Created on: 11 mar 2018
+ *      Author: Matteo Tesori
+ */
+
+#include <stdio.h>
+int main(){
+	printf("Hello World and Github, this is my very first repo!");
+	return 0;
+}
